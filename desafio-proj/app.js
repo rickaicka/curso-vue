66ff75db83c7419b636aaa7674b5433ee951c6e0
Ricardo Salim <ricardosalim7@hotmail.com>
2019-09-13 03:14:07 -0300

    'steps'

---
+++ desafio-proj/app.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app',
+    data: {
+        playerLife: 0,
+        monsterLife: 10
+    },
+    computed: {
+        hasResult(){
+            return this.playerLife == 0 || this.monsterLife == 0;
+        }
+    }
+})
